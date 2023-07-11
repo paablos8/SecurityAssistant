cbab1eb443a54f4663d57755b8f667514f0dd037
schimslady <tschaible@web.de>
2023-07-11 15:43:13 +0200

    DatenbankZugriff

--- src/main/java/com/example/SecurityAssistant/controller/submitController.java
+++ src/main/java/com/example/SecurityAssistant/controller/submitController.java
@@ -1,18 +1,23 @@
 package com.example.SecurityAssistant.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.SecurityAssistant.entities.SecurityInfrastructure;
 import com.example.SecurityAssistant.repository.InfrastructureRepository;
 
 @Controller
 public class submitController {
+
+    long eintragNR;
 
     @Autowired
     private InfrastructureRepository repo;
@@ -31,9 +36,20 @@ public class submitController {
         model.addAttribute("Firewall", infra.getFirewall());
 
         repo.save(infra);
+        System.out.println(repo.count());
+        eintragNR = repo.count();
 
         return "success";
     } 
+
+    /* @RequestMapping("/success")
+     public ModelAndView showData(){
+        ModelAndView mv = new ModelAndView();
+        List <SecurityInfrastructure> dataList = repo.findAll();
+        mv.addObject("data",dataList);
+        mv.setViewName("success");
+        return mv;
+     } */
 
 
 }
