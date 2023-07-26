f4e00bdb5d49ae751185c3e09809849ffd898834
schimslady <tschaible@web.de>
2023-07-26 18:57:07 +0200

    error 404 and tooltip style

---
+++ src/main/java/com/example/SecurityAssistant/controller/errorController.java
@@ -0,0 +1,16 @@
+package com.example.SecurityAssistant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class errorController {
+
+    @GetMapping("/error")
+    public String handleError() {
+        return "error"; // Returns the error.html template
+    }
+}
+    
+
